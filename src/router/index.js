eee66fd7cfc5a0bfe4877eca3e63656ebcaee057
Muhammadjon <muhammadzonboborahimov7@gmail.com>
2026-04-22 17:58:35 +0500

    router fix

--- src/router/index.js
+++ src/router/index.js
@@ -27,19 +27,19 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to) => {
   const token = localStorage.getItem("token");
 
   // если не залогинен — только /login доступен
   if (!token && to.path !== "/price-calc/login") {
-    return next("/price-calc/login");
+    return "/price-calc/login";
   }
 
   // если залогинен — не пускаем на login
   if (token && to.path === "/login") {
-    return next("/price-calc/");
+    return "/price-calc/";
   }
 
-  next();
+  return true;
 });
 export default router;
